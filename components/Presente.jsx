289f27815d5760503cf14e3e0b8fef38f05960ca
GMontanher96 <gabrielmontanherr@gmail.com>
2023-01-31 17:07:41 -0200

    Create component present and details

--- components/Presente.jsx
+++ components/Presente.jsx
@@ -3,8 +3,11 @@ import styles from "../styles/Presente.module.css"
 
 export default function Presente() {
     return (
-        <div>
-
+        <div className={styles.presente}>
+            <div className={styles.topo}></div>
+            <div className={styles.corpo}></div>
+            <div className={styles.laco1}></div>
+            <div className={styles.laco2}></div>
         </div>
     )
 }
